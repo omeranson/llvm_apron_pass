dc2f7c37a0b3c3813dbe9f0d21e3230465174c39
Omer Anson <oaanson@gmail.com>
2017-03-26 22:28:48 +0300

    Initial docker support

--- environment.sh
+++ environment.sh
@@ -4,3 +4,4 @@ export APRON_LIB=$APRON_INSTALL/lib
 export LLVM_INSTALL=$HOME/opt/llvm-install
 export LLVM_LIB=$LLVM_INSTALL/lib
 export LD_LIBRARY_PATH=$LLVM_LIB:$APRON_LIB
+export LLVM_OPT=$LLVM_INSTALL/bin/opt
