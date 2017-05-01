954745dceebc61facdadef924baf06d56ae6af06
Omer Anson <oaanson@gmail.com>
2017-05-01 13:05:25 +0300

    Correct implementation of ApronAbstractState::operator<=

--- ApronPass/src/AbstractStates/ApronAbstractState.cpp
+++ ApronPass/src/AbstractStates/ApronAbstractState.cpp
@@ -305,20 +305,16 @@ bool ApronAbstractState::operator!=(const ApronAbstractState &other) const {
 }
 
 bool ApronAbstractState::operator<=(const ApronAbstractState & other) const {
-	ap_abstract1_t this_abst = m_abstract1;
-	ap_abstract1_t other_abst = other.m_abstract1;
-	ap_dimchange_t * dimchange1 = NULL;
-	ap_dimchange_t * dimchange2 = NULL;
-	ap_environment_t * environment = ap_environment_lce(
-			ap_abstract1_environment(apron_manager, &this_abst),
-			ap_abstract1_environment(apron_manager, &other_abst),
-			&dimchange1, &dimchange2);
-
-	this_abst = ap_abstract1_change_environment(
-			apron_manager, false, &this_abst, environment, true);
-	other_abst = ap_abstract1_change_environment(
-			apron_manager, false, &other_abst, environment, false);
-	return ap_abstract1_is_leq(apron_manager, &this_abst, &other_abst);
+	ap_environment_t * my_env = (ap_environment_t*)getEnvironment();
+	ap_environment_t * other_env = (ap_environment_t*)other.getEnvironment();
+	if (!ap_environment_is_leq(my_env, other_env)) {
+		return false;
+	}
+	ap_abstract1_t this_abst = ap_abstract1_change_environment(
+			apron_manager, false,
+			(ap_abstract1_t*)&m_abstract1, other_env, true);
+	return ap_abstract1_is_leq(apron_manager, &this_abst,
+			(ap_abstract1_t*)&other.m_abstract1);
 }
 
 ap_texpr1_t * ApronAbstractState::asTexpr(const std::string & var) {
