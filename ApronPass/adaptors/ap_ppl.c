ebf7f8726fcf341e21e67718dad3ff2d51cfc388
Omer Anson <oaanson@gmail.com>
2017-05-02 16:49:19 +0300

    Make apron errors exit process with failure (abort)

--- ApronPass/adaptors/ap_ppl.c
+++ ApronPass/adaptors/ap_ppl.c
@@ -3,6 +3,10 @@
 #include <ap_ppl.h>
 
 ap_manager_t * create_manager() {
-	return ap_ppl_poly_manager_alloc(true);
+	ap_manager_t * manager = ap_ppl_poly_manager_alloc(true);
+	for (int i=0; i<AP_EXC_SIZE; i++) {
+		ap_manager_set_abort_if_exception(manager, (ap_exc_t)i, true);
+	}
+	return manager;
 }
 
